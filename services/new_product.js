e1b8c7643fcc3000c73a148f9f4ec0c924cba078
Lance Jervis B. Tuazon <ltuazon@gbox.adnu.edu.ph>
2025-10-09 09:44:08 +0800

    Update new_product.js

--- services/new_product.js
+++ services/new_product.js
@@ -1,8 +1,19 @@
 import Product from "..\models\product_model.js";
 
-const addnewProduct = async () => {
+const addnewProduct = async (Product) => {
     return await Product.find();
 }
+
+const updateProduct = async (id, Product) => {
+    return await Product.findByIdAndUpdate(id, Product, { new: true });
+}
+
+const deleteProduct = async (id) => {
+    return await Product.findByIdAndDelete(id);
+}
+
 export default {
-    addnewProduct
+    addnewProduct,
+    updateProduct,
+    deleteProduct
 }
